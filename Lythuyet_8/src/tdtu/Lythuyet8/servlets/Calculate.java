5b511de44164af084a3f5b2ed438e58b9c001df2
punkrocker178 <ngochieu.le1708@gmail.com>
2020-03-16 21:44:43 +0700

    Finish lythuyet8
    Cheezy way

--- Lythuyet_8/src/tdtu/Lythuyet8/servlets/Calculate.java
+++ Lythuyet_8/src/tdtu/Lythuyet8/servlets/Calculate.java
@@ -8,35 +8,78 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.LinkedList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Calculate extends javax.servlet.http.HttpServlet {
 
-    private String calculate(String s) {
+    private Double calculate(String s) {
 
         Pattern numPattern = Pattern.compile("\\d+");
         Pattern operator = Pattern.compile("[^\\w]+");
         Matcher m = numPattern.matcher(s);
         Matcher m2 = operator.matcher(s);
-        ArrayList numAl = new ArrayList();
-        ArrayList operatorAl = new ArrayList();
+        ArrayList<String> numList = new ArrayList<String>();
+        ArrayList<String> operatorList = new ArrayList<String>();
 
         while(m.find()) {
-            numAl.add(m.group().trim());
+            numList.add(m.group().replace("\\s+", ""));
         }
 
         while(m2.find()) {
-            operatorAl.add(m2.group().trim());
+            operatorList.add(m2.group().replace("\\s+", ""));
         }
 
-        String res = numAl.toString() + operatorAl.toString();
-        return res;
+
+        // 2*3-5
+        Double result = 0.0;
+        int i = 0, j = 0;
+        while(i < numList.size() && j < operatorList.size()) {
+            if (i == 0) {
+                switch(operatorList.get(i)) {
+                    case "+":
+                        result = Double.parseDouble(numList.get(i)) + Double.parseDouble(numList.get(i+1));
+                        break;
+                    case "-":
+                        result = Double.parseDouble(numList.get(i)) - Double.parseDouble(numList.get(i+1));
+                        break;
+                    case "*":
+                        result = Double.parseDouble(numList.get(i)) * Double.parseDouble(numList.get(i+1));
+                        break;
+                    case "/": case ":":
+                        result = Double.parseDouble(numList.get(i)) / Double.parseDouble(numList.get(i+1));
+                        break;
+                }
+            } else {
+                switch(operatorList.get(i)) {
+                    case "+":
+                        result += Double.parseDouble(numList.get(i+1));
+                        break;
+                    case "-":
+                        result -= Double.parseDouble(numList.get(i+1));
+                        break;
+                    case "*":
+                        result *= Double.parseDouble(numList.get(i+1));
+                        break;
+                    case "/": case ":":
+                        result /= Double.parseDouble(numList.get(i+1));
+                        break;
+                }
+            }
+
+            i++;
+            j++;
+
+        }
+
+
+        return result;
     }
 
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         PrintWriter out = response.getWriter();
-        String s = this.calculate(request.getParameter("expression"));
+        Double s = this.calculate(request.getParameter("expression"));
         out.println("<html><body>" + s + "</body></html>");
     }
 
